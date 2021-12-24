a9ec6e6fc2c663a75f1ee5b5127ed9e4138c4b53
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2021-12-24 09:47:04 +0530

    Update read_json_from_S3.py

--- read_json_from_S3.py
+++ read_json_from_S3.py
@@ -4,22 +4,21 @@ s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     # TO DO implement
-    print(event)
     
-    # Print S3 bucket name
+    #1 - Print S3 bucket name
     bucket = event['Records'][0]['s3']['bucket']['name']
     print(bucket)
     
-    # Print .json filename
+    #2 - Print .json filename
     json_file = event['Records'][0]['s3']['object']['key']
     print(json_file)
     
-    # Print file objects(metadata) of json_file
+    #3 - Print file objects(metadata) of json_file
     json_file_obj = s3_client.get_object(Bucket=bucket, Key=json_file)
     print(json_file_obj)
     
-    # Print the file content
-    lines = json_file_obj['Body'].read().decode('utf-8').split()
+    #4 - Print the file content
+    lines = json_file_obj['Body'].read().decode('utf-8')
     print(lines)
     
     return {
