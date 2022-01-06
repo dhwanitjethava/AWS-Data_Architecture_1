149f774ee3f0533ab118c7d8654757db9fe5f6d4
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2022-01-06 12:04:32 +0530

    Update automatic_s3_uploader.py

--- automatic_s3_uploader.py
+++ automatic_s3_uploader.py
@@ -1,19 +1,14 @@
-# Import AWS credentials from aws_secret_key.py
-from aws_secret_key import access_key, secret_access_key
-
 import boto3
 import os
 
-client = boto3.client('s3',
-                        aws_access_key_id = access_key,
-                        aws_secret_access_key = secret_access_key)
+# Import AWS credentials from aws_secret_key.py
+from aws_secret_key import access_key, secret_access_key
 
-for file in os.listdir('/Users/dhwanitjethava/Desktop/AWS_Data_Pipeline/Dataset/'):
-    data = '/Users/dhwanitjethava/Desktop/AWS_Data_Pipeline/Dataset/Employees_3_1.json'
+client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
 
-    if '.json' in file:
+for files in os.listdir():
+    
+    if '.json' in files:
         upload_file_bucket = 'aws-learning-scaletech'
-        upload_file_key = 'JSON_files/' + str(data)
-        client.upload_file(data, upload_file_bucket, upload_file_key)
-    else:
-        print('Error: File is not found!')
+        upload_file_key = 'JSON_files/' + str(files)
+        client.upload_file(files, upload_file_bucket, upload_file_key)
