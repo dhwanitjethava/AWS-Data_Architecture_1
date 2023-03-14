6dec8bbc979106bf68dece48fa47a88c92c285dd
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-03-14 09:43:18 +0530

    Update automatic_s3_uploader.py

--- automatic_s3_uploader.py
+++ automatic_s3_uploader.py
@@ -1,3 +1,4 @@
+# Required libraries
 import boto3
 import os
 
@@ -6,8 +7,7 @@ from aws_secret_key import access_key, secret_access_key
 
 client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
 
-for files in os.listdir():
-    
+for files in os.listdir():   
     if '.json' in files:
         upload_file_bucket = '<bucket_name>'
         upload_file_key = 'JSON_files/' + str(files)
