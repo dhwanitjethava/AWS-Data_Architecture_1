c00cfe010055f6610e363c971f66e68a6433ea1e
Dhwanit Jethava <96478746+dhwanitjethava@users.noreply.github.com>
2023-03-14 09:40:33 +0530

    Delete verify_lambda_function.py

--- verify_lambda_function.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # To do Implement
-    print(event)
-    return {
-        'statusCode' : 200,
-        'body' : json.dump('Hello from Lambda!')
-    }
